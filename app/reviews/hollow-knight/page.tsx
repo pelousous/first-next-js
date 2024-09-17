336fdf32408d88b73abc9a2b512fc1810e2c5da1
davide <info@davideravasi.com>
2024-09-17 07:30:47 +0000

    feat: add all files

--- app/reviews/hollow-knight/page.tsx
+++ app/reviews/hollow-knight/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Heading from "../../../components/Heading";
+import Heading from "@/components/Heading";
 
 export default function HollowKnightPage() {
   return (
     <>
       <Heading>Hollow Knight</Heading>
+      <img src="/images/hollow-knight.jpg" alt="" width="640" height="360" className="mb-2 rounded" />
       <p>Here we'll list all the reviews</p>
     </>
   );
--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,22 +1,27 @@
 import Link from "next/link";
 import React from "react";
-import Heading from "../../components/Heading";
+import Heading from "@/components/Heading";
 
 export default function ReviewsPage() {
   return (
     <>
       <Heading>Reviews</Heading>
       <nav>
-        <ul>
-          <li>
-            <Link href="/reviews/hollow-knight">Hollow knight</Link>
+        <ul className="flex flex-col gap-4">
+          <li className="bg-white border rounded pb-1 shadow hover:shadow-xl">
+            <Link href="/reviews/hollow-knight">
+              <img src="/images/hollow-knight.jpg" alt="" width="640" height="360" className="mb-2 rounded-t" />
+              <h2 className="text-center">Hollow knight</h2>
+            </Link>
           </li>
-          <li>
-            <Link href="/reviews/stardew-valley">Stardew valley</Link>
+          <li className="bg-white border rounded pb-1 shadow hover:shadow-xl">
+            <Link href="/reviews/stardew-valley">
+              <img src="/images/stardew-valley.jpg" alt="" width="640" height="360" className="mb-2 rounded-t" />
+              <h2 className="text-center">Stardew Valley</h2>
+            </Link>
           </li>
         </ul>
       </nav>
-      <p>Here we'll list all the reviews</p>
     </>
   );
 }
